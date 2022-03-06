81b357a756dcf2709f89444195444d47d08a1aec
geonil2 <kji8922@naver.com>
2022-03-06 19:32:06 +0900

    develop contract

--- contracts/IMMMMarket.sol
+++ contracts/IMMMMarket.sol
@@ -24,21 +24,29 @@ contract IMMMMarket {
     // 
     mapping (address => Counters.Counter) private _userListIdsCount;
 
+
     function userListCount(address owner) public view returns (uint256) {
-        require(owner != address(0), "owner is zero address");
+        require(owner != address(0), "Owner is zero address");
 
         return _userListIdsCount[owner].current();
     }
 
-    function addList (address token, uint256 tokenId, address seller, uint256 buyPrice, uint256 timeStamp) public {
+    function addList(address token, uint256 tokenId, uint256 buyPrice, uint256 timeStamp) public returns (bool) {
+        // seller == msg.sender
         IKIP17(token).safeTransferFrom(msg.sender, address(this), tokenId, '0x00');
-
+        
         uint256 listId = _listNum.current();
 
-        _lists[listId] = list(tokenId, seller, buyPrice, timeStamp);
-        _userListIdsCount[seller].increment();
+        if(listId == 0) {
+            _listNum._value = 1;
+        }
+
+        _lists[listId] = list(tokenId, msg.sender, buyPrice, timeStamp);
+        _userListIdsCount[msg.sender].increment();
         
         _listNum.increment();
+
+        return true;
     }
 
     function buyList(address token, uint256 listId) public payable returns (bool) {
@@ -48,9 +56,34 @@ contract IMMMMarket {
 
         receiver.transfer(list.buyPrice);
         IKIP17(token).safeTransferFrom(address(this), msg.sender, list.tokenId, '0x00');
+
+        return true;
     }
+
+    function cancelList(address token, uint256 listId) public returns (bool) {
+        list memory list = _lists[listId];
+
+        require(list.seller == msg.sender, "Your not owner");
+        IKIP17(token).safeTransferFrom(address(this), list.seller, list.tokenId, '0x00');
+
+        return true;
+    }
+
+    // function closeList(address seller, uint256 listId) internal {
+    //     _userListIdsCount[seller].decrement();
+    //     uint256 lastListCount = _userListIdsCount[seller].current();
+
+    //     for(uint256 i = 0; i < lastListCount; i++) {
+
+    //     }
+    //     // delete _lists[listId];
+
+    // }
+
 
     function onKIP17Received(address operator, address from, uint256 tokenId, bytes memory data) public returns (bytes4) {
         return bytes4(keccak256("onKIP17Received(address,address,uint256,bytes)"));
     }
+
+
 }
